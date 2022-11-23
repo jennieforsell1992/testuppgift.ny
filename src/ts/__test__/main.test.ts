f45d6067975b573fe0590fb1a9462d5132e0f7a2
jennie Forsell <forselljennie@gmail.com>
2022-11-23 14:21:54 +0100

    gjorde klart testet på addTodo

--- src/ts/__test__/main.test.ts
+++ src/ts/__test__/main.test.ts
@@ -53,22 +53,35 @@ describe("changeTodoTest", () => {
   });
 });
 
-test("add Todo to a list ", () => {
-  //Arrange
+describe("Should push a todo to my list or get a warning", () => {
+  test("add Todo to a list ", () => {
+    //Arrange
+    let todos: Todo[] = [];
+    let todoText: string = "hej på dig!";
+    //min todos längd.
+    let length: number = todos.length;
 
-  let todos: Todo[] = [];
-  let todoText: string = "hej på dig!";
-  let length: number = todos.length;
-  //todos.push(todoText);
-  //let newTodo: Todo = new Todo("",false);
+    //Act
+    addTodo(todoText, todos);
+    console.log(todos);
 
-  //Act
-  addTodo(todoText, todos);
-  console.log(todos);
+    //Assert
+    console.log(todos);
+    expect(todos.length).toBe(length + 1);
+  });
 
-  //Assert
-  console.log(todos);
-  expect(todos.length).toBe(length + 1);
+  test("should give a warning", () => {
+    //Arrange
+    let todos: Todo[] = [];
+    let todoText: string = "he";
+    let length: number = todos.length;
+    //Act
+
+    let result = addTodo(todoText, todos);
+    console.log(todos);
+    console.log(result.error);
+
+    //Assert
+    expect(todos.length).toBe(length);
+  });
 });
-
-test("should give a warning", () => {});
